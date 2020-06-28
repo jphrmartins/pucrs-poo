ee85425fc5162c1fea235bd7fdafeba0e0426810
jpmartins <joao@ubots.com.br>
2020-06-28 16:18:05 -0300

    changing rule of mainmenuOptions

--- src/app/App.java
+++ src/app/App.java
@@ -6,9 +6,9 @@ public class App {
     public static void main(String[] args) {
 
         Menu menu = new Menu();
-        int option = 0;
+        int option = 1;
 
-        while (option != 4) {
+        while (option >=1 && option <= 4) {
             MenuOptions.mainOptions();
             option = menu.readReply();
             menu.dashBoard(option);
